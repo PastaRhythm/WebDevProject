f21476dd89dbc5d0ccb442eaa1f479da6b8ab1fb
PastaRhythm <connor.felton@gmail.com>
2023-11-16 22:16:51 -0500

    Lots of backend
    Forms for creating a site and uploading files. Routes for all of that, as well as viewing a site.

---
+++ forms/siteForms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import SubmitField, StringField, SelectField, FileField
+from wtforms.validators import InputRequired, Regexp
+
+class NewSiteForm(FlaskForm):
+    host_name = StringField("Host Name: ", validators=[InputRequired()])
+    # pricing_plan = SelectField("Pricing Plan: ", coerce=int)
+
+    submit = SubmitField("Register")
+
+class UploadFilesForm(FlaskForm):
+    # This gives a file path
+    zip_file = FileField('Zip File, preferably containing index.html at root', validators = [InputRequired(), Regexp('^[^/\\]\.zip$')])
+
+    submit = SubmitField("Set website files to selected")
