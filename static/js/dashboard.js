3f9875d6ab676118671703f0e4aa7a76fc0a4344
Jacob <jtgraham38@gmail.com>
2023-12-05 23:08:25 -0500

    Loade the content for the two tabs correctly.

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -55,4 +55,191 @@ async function populate_dashboard_body(clicked_tab){
     
     //place the body in the body div
     body.innerHTML = body_content
+
+    //execute the appropriate scripts, based on the chosen tab
+    if (clicked_tab.id == "dashboard_my_sites_tab"){
+        console.log("execute scripts for my sites tab")
+        fetch_user_sites()
+    } else if (clicked_tab.id == "dashboard_shared_sites_tab"){
+        console.log("execute scripts for shared sites tab")
+        fetch_shared_sites()
+    }
 }
+
+//functions for rendering the user's sites table
+async function fetch_user_sites(){
+    const user_websites_tbody = document.getElementById('user_websites_tbody')
+    const endpoint = `/sites_data`
+    console.log(endpoint)
+
+    //add loader
+    user_websites_tbody.appendChild(create_loader())
+
+    //get data
+    const response = await fetch(endpoint)
+    const websites = await validateJSON(response)
+    console.log(websites)
+    console.log("done")
+
+    //clear current children, and add loader
+    user_websites_tbody.innerHTML = ""  //clear all children
+
+    //insert data into table
+    websites.map((website)=>{
+        //create row
+        const tr = document.createElement('tr')
+        
+        //add site name col
+        const site_name = document.createElement('td')
+        site_name.innerText = website.name
+        tr.appendChild(site_name)
+
+        //add site id col
+        const site_id = document.createElement('td')
+        site_id.innerText = website.id
+        tr.appendChild(site_id)
+
+        //add image col
+        const site_image = document.createElement('td')
+        site_image.innerText = website.image
+        tr.appendChild(site_image)
+
+        //add visit link col
+        const site_visit = document.createElement('td')
+        const site_link = document.createElement('a')
+        site_link.innerText = "Visit"
+        site_link.href = "http://" + website.hostname
+        site_visit.append(site_link)
+        tr.appendChild(site_visit)
+
+        //Add file upload link col
+        const site_upload = document.createElement('td')
+        const upload_link = document.createElement('a')
+        upload_link.innerText = "Upload"
+        upload_link.href = "/upload_files/" + website.id + "/"
+        site_upload.append(upload_link)
+        tr.appendChild(site_upload)
+
+        //Add share link col
+        const site_share = document.createElement('td')
+        const share_link = document.createElement('a')
+        share_link.innerText = "Share"
+        share_link.href = "/share_site/" + website.id + "/"
+        site_share.append(share_link)
+        tr.appendChild(site_share)
+
+        //add the delete button
+        const del_btn = document.createElement('button')
+        del_btn.addEventListener('click', (event)=>{
+            delete_site(website.id)
+        })
+        const icon = document.createElement('i')
+        icon.classList.add('fa-solid')
+        icon.classList.add('fa-trash')
+        del_btn.appendChild(icon)
+        tr.appendChild(del_btn)
+
+        //add tr to body
+        user_websites_tbody.appendChild(tr)
+    })
+}
+
+async function delete_site(id){
+    console.log('deleting site!')
+
+    //create and add loader
+    const user_websites_tbody = document.getElementById('user_websites_tbody')
+    user_websites_tbody.innerHTML = ""
+    user_websites_tbody.appendChild(create_loader())
+
+    //delete record and get results
+    fetch("/delete_site/" + id + '/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        //body: JSON.stringify(dataToSend),
+        })
+        .then(response => {
+            //check if the request was successful (status code 2xx)
+            if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            //if the response is ok, refresh the table
+            console.log('refreshing table')
+            fetch_user_sites()
+        })
+        .catch(error => {
+            //handle errors during the fetch
+            console.error('Fetch error:', error);
+        });
+}
+
+//end functions for rendering the user's sites table
+//functions for rendering the user's sites table
+
+async function fetch_shared_sites(){
+    const user_websites_tbody = document.getElementById('shared_websites_tbody')
+    const endpoint = `/shared_sites_data`
+    console.log(endpoint)
+
+    //add loader
+    user_websites_tbody.appendChild(create_loader())
+
+    //get data
+    const response = await fetch(endpoint)
+    const websites = await validateJSON(response)
+    console.log(websites)
+    console.log("done")
+
+    //clear current children, and add loader
+    user_websites_tbody.innerHTML = ""  //clear all children
+
+    //insert data into table
+    websites.map((website)=>{
+        //create row
+        const tr = document.createElement('tr')
+        
+        //add site name col
+        const site_name = document.createElement('td')
+        site_name.innerText = website.name
+        tr.appendChild(site_name)
+
+        //add site id col
+        const site_id = document.createElement('td')
+        site_id.innerText = website.id
+        tr.appendChild(site_id)
+
+        //add image col
+        const site_image = document.createElement('td')
+        site_image.innerText = website.image
+        tr.appendChild(site_image)
+
+        //add visit link col
+        const site_visit = document.createElement('td')
+        const site_link = document.createElement('a')
+        site_link.innerText = "Visit"
+        //site_link.target = "_blank" //open in new tab
+        site_link.href = "http://" + website.hostname
+        site_visit.append(site_link)
+        tr.appendChild(site_visit)
+
+        //Add file upload link col
+        const site_upload = document.createElement('td')
+        const upload_link = document.createElement('a')
+        upload_link.innerText = "Upload"
+        upload_link.href = "/upload_files/" + website.id + "/"
+        site_upload.append(upload_link)
+        tr.appendChild(site_upload)
+
+        //add owner col
+        const site_owner = document.createElement('td')
+        site_owner.innerText = website.owner_name
+        tr.appendChild(site_owner)
+
+        //add tr to body
+        user_websites_tbody.appendChild(tr)
+    })
+}
+
+//end functions for rendering the user's sites table
