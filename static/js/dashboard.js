275e34bccfa37916d42996817a742d3bcf720250
Jacob <jtgraham38@gmail.com>
2023-12-08 13:44:42 -0500

    Begin switching from site table to site cards.

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -68,7 +68,7 @@ async function populate_dashboard_body(clicked_tab){
 
 //functions for rendering the user's sites table
 async function fetch_user_sites(){
-    const user_websites_tbody = document.getElementById('user_websites_tbody')
+    const user_websites_tbody = document.getElementById('user_sites_dashboard_container')
     const endpoint = `/sites_data`
     console.log(endpoint)
 
@@ -86,47 +86,72 @@ async function fetch_user_sites(){
 
     //insert data into table
     websites.map((website)=>{
-        //create row
-        const tr = document.createElement('tr')
+        //create card
+        const card = document.createElement('div')
+        card.classList.add('card')
         
         //add site name col
-        const site_name = document.createElement('td')
+        const site_name = document.createElement('h5')
+        site_name.classList.add('card-title')
         site_name.innerText = website.name
-        tr.appendChild(site_name)
+        card.appendChild(site_name)
 
         //add site id col
-        const site_id = document.createElement('td')
-        site_id.innerText = website.id
-        tr.appendChild(site_id)
+        // const site_id = document.createElement('td')
+        // site_id.innerText = website.id
+        // card.appendChild(site_id)
+        const card_body = document.createElement('div')
+        card_body.classList.add('card-body')
+        card.appendChild(card_body)
 
         //add image col
-        const site_image = document.createElement('td')
-        site_image.innerText = website.image
-        tr.appendChild(site_image)
+        // const site_image = document.createElement('td')
+        // site_image.innerText = website.image
+        // card.appendChild(site_image)
+        const card_actions = document.createElement('div')
+        card_actions.classList.add('card-footer')
+        card_actions.classList.add('d-flex')
+        card_actions.classList.add('flex-row')
+        card_actions.classList.add('justify-content-end')
+        card_actions.classList.add('align-items-center')
+        card_actions.classList.add('gap-2')
+        card.appendChild(card_actions)
 
-        //add visit link col
-        const site_visit = document.createElement('td')
-        const site_link = document.createElement('a')
-        site_link.innerText = "Visit"
-        site_link.href = "http://" + website.hostname
-        site_visit.append(site_link)
-        tr.appendChild(site_visit)
+        //add visit link
+        const visit_link = document.createElement('a')
+        const visit_icon = document.createElement('i')
+        visit_icon.classList.add('fa-eye')
+        visit_icon.classList.add('fa-solid')
+        visit_link.appendChild(visit_icon)
+        visit_link.href = "http://" + website.hostname
+        card_actions.appendChild(visit_link)
 
-        //Add file upload link col
-        const site_upload = document.createElement('td')
+        //Add file upload link
         const upload_link = document.createElement('a')
-        upload_link.innerText = "Upload"
+        const upload_icon = document.createElement('i')
+        upload_icon.classList.add('fa-upload')
+        upload_icon.classList.add('fa-solid')
+        upload_link.appendChild(upload_icon)
         upload_link.href = "/upload_files/" + website.id + "/"
-        site_upload.append(upload_link)
-        tr.appendChild(site_upload)
+        card_actions.appendChild(upload_link)
 
-        //Add share link col
-        const site_share = document.createElement('td')
+        //add terminal link
+        const terminal_link = document.createElement('a')
+        const terminal_icon = document.createElement('i')
+        terminal_icon.classList.add('fa-terminal')
+        terminal_icon.classList.add('fa-solid')
+        terminal_link.appendChild(terminal_icon)
+        terminal_link.href = "/#"
+        card_actions.appendChild(terminal_link)
+
+        //Add share link
         const share_link = document.createElement('a')
-        share_link.innerText = "Share"
+        const share_icon = document.createElement('i')
+        share_icon.classList.add('fa-share')
+        share_icon.classList.add('fa-solid')
+        share_link.appendChild(share_icon)
         share_link.href = "/share_site/" + website.id + "/"
-        site_share.append(share_link)
-        tr.appendChild(site_share)
+        card_actions.appendChild(share_link)
 
         //add the delete button
         const del_btn = document.createElement('button')
@@ -139,10 +164,10 @@ async function fetch_user_sites(){
         icon.classList.add('fa-solid')
         icon.classList.add('fa-trash')
         del_btn.appendChild(icon)
-        tr.appendChild(del_btn)
+        card_actions.appendChild(del_btn)
 
         //add tr to body
-        user_websites_tbody.appendChild(tr)
+        user_websites_tbody.appendChild(card)
     })
 }
 
